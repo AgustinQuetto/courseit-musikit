a72753f10735f5849bdfa586eaf0939c8a63a776
Agustín Quetto <agustinquetto@gmail.com>
2019-11-12 19:19:45 -0300

    Buffer to Buffer.from

--- controllers/SpotifyController.js
+++ controllers/SpotifyController.js
@@ -2,42 +2,43 @@ const Axios = require("axios");
 const querystring = require("querystring");
 
 class SpotifyController {
-    constructor(clientId, secretId, redirect_uri) {
-        this.clientId = clientId;
-        this.secretId = secretId;
-        this.redirect_uri = redirect_uri;
-    }
+  constructor(clientId, secretId, redirect_uri) {
+    this.clientId = clientId;
+    this.secretId = secretId;
+    this.redirect_uri = redirect_uri;
+  }
 
-    async login(code) {
-        console.log(code);
-        const spotifyUrl = "https://accounts.spotify.com/api/token";
+  async login(code) {
+    console.log(code);
+    const spotifyUrl = "https://accounts.spotify.com/api/token";
 
-        const params = {
-            headers: {
-                Authorization:
-                    "Basic " +
-                    new Buffer(`${this.clientId}:${this.secretId}`).toString(
-                        "base64"
-                    ),
-                "Content-Type": "application/x-www-form-urlencoded"
-            }
-        };
-        try {
-            const login = await Axios.post(
-                spotifyUrl,
-                querystring.stringify({
-                    grant_type: "authorization_code",
-                    redirect_uri: "http://localhost:3000/",
-                    code: code
-                }),
-                params
-            );
-            return login.data;
-        } catch (e) {
-            console.log(e);
-            return { error: "spotify login error" };
-        }
+    const params = {
+      headers: {
+        Authorization:
+          "Basic " +
+          new Buffer.from(
+            `${this.clientId}:${this.secretId}`,
+            "utf-8"
+          ).toString("base64"),
+        "Content-Type": "application/x-www-form-urlencoded"
+      }
+    };
+    try {
+      const login = await Axios.post(
+        spotifyUrl,
+        querystring.stringify({
+          grant_type: "authorization_code",
+          redirect_uri: "http://localhost:3000/",
+          code: code
+        }),
+        params
+      );
+      return login.data;
+    } catch (e) {
+      console.log(e);
+      return { error: "spotify login error" };
     }
+  }
 }
 
 module.exports = SpotifyController;
